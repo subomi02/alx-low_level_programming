341f45a981ef99f1b0a94bf8d8e91aeb3a7bfb44
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-06-30 20:10:57 -0700

    Updated puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-	puts();
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
-void puts(){
-	printf("Programming is like building a multilingual puzzle");
-}
-
