dd55e993c8bfae48d4b99e87a6c87a48f41b848e
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-06-30 21:54:38 -0700

    Create a c file to print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
