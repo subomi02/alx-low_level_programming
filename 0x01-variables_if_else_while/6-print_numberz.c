9ead6eff760c702518de296e8a578132395e6bb8
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-01 18:46:52 -0700

    Created a script to print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print all single digits in base 10
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
