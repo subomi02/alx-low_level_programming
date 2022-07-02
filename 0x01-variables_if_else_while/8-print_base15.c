8bf7444c0be9361e6fb0e59df711c343e4c34490
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-01 20:18:45 -0700

    Created a script to print all numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base15.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - output all single digits of base 16 in lowercase
+ *
+ * Return: always 0 (sucess)
+ */
+
+int main(void)
+{
+	int b;
+	char B;
+
+	for (b = 0; b < 10; b++)
+	{
+		putchar((var % 10) + '0')
+	}
+
+	for (B = 'a'; b <= 'f'; b++)
+	{
+		putchar(B)
+	}
+	putchar('\n');
+	return (0);
+}
