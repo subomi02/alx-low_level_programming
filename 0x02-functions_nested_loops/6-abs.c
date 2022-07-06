e51e1c5e8b114dda253d1b6f065b6bf45ec37ee3
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-05 21:42:39 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @b: input
+ *
+ * Return: the absolute value of the input
+ */
+
+int _abs(int b)
+{
+	return ((b <= 0) ? b * -1 : n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,5 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int b);
 int _isalpha(int b);
-int print_sign(int n);
+int print_sign(int b);
+int _abs(int);
