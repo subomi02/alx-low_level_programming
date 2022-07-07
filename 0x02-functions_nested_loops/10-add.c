01832cf30fb3aec3537da039c7be1d2600d8a524
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-06 17:57:40 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two number
+ *
+ *@a: first number
+ *@b:second number
+ * Return: the sum of the two numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
