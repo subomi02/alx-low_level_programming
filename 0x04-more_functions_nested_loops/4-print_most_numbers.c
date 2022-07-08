2eae9997d7fc19d8aa2c2a95706af9aed93196b6
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-07 19:34:33 -0700

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_most_numbers - multiplies two numbers
+* Return: 0
+*/
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a == 2 || a == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(a + '0');
+		}
+	}
+	_putchar('\n');
+}
