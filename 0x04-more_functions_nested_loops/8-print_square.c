32b7391e0a0671726d5ef8bf8b8532bc9db63cb7
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-07 20:51:48 -0700

    8-print-square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_line - print lines
+* @a:number of lines printed
+* Return: 0
+*/
+
+void print_line(int a)
+{
+	int b;
+
+	if (a > 0)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
