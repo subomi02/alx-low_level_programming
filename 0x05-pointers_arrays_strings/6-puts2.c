9ea71cc7984856047a60105525dc0071e83365f7
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-13 15:46:44 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*puts2 - prints the even numbers out of a string
+*@str: string to be printed in evens
+* Return: Always 0.
+*/
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
+	}
+	putchar('\n');
+}
