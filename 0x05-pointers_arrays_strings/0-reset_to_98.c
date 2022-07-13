506051c586377f49ebde93cf87e93bce750a7c8f
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-13 11:01:40 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* reset_to_98 - reset a number to 98
+*
+* Return: Always 0.
+*/
+
+void reset_to_98(int *a)
+{
+	*a = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Created READM.md
