6c8a5496f1b99eb0dcc71d34eafe159ad1808062
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-13 17:38:42 -0700

    9-strcpy,c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+* _strcpy - copy string from src to dest
+* @dest: pointer to the string copied from src
+* @src: pointer to the string to be copied
+*
+* Return: pointer to the copied string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
