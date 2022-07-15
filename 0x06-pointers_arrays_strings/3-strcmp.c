76c0e07e8691f973c28084a080cea81cc08d40d4
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-14 18:51:59 -0700

    rev_array.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-/**
-*_strcmp - appends the content of second to first with limit to n
-*@fir:first variable
-*@sec:second variable
-* Return: the result of the combination
+#include<string.h>
+/*
+*_strcmp - compares first and second
+* @s1:first
+* @s2:second
+* return: 1,-1 or 0
 */
-int *_strcmp(char *fir, char *sec)
+
+int _strcmp(char *s1, char *s2)
 {
-	return (strcmp(fir, sec));
+	return (strcmp(s1, s2));
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ char *_strcat(char *fir, char *sec);
 char *_strncat(char *fir, char *sec, int n);
 char *_strncpy(char *fir, char *sec, int n);
 int *_strcmp(char *fir, char *sec);
+void reverse_array(int *a, int n);
