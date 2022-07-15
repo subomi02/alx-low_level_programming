f04c924da8a21e9bb3990d991f020faffb9389bc
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-14 17:57:23 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*_strcat - appends the content of second to first
+*@fir:first variable
+*@sec:second variable
+* Return: the result of the combination
+*/
+char *_strcat(char *fir, char *sec)
+{
+	return (strcat(fir, sec));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,1 @@
+char *_strcat(char *fir, char *sec);
