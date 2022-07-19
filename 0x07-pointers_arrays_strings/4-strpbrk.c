7cd347edea44e41e46a3b4f51a98db193df98fa4
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-18 17:15:03 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*_strpbrk - function that locates a character in a string
+*@s:string
+*@accept:character to be located
+*Return: pointer to the first occurence of character c in string s
+*/
+char _strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *s, char *b, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strkpbrk(char *s, char *accept);
