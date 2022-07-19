280507b97582e50799965429ba70d0d88008e00e
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-18 17:28:38 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,7 +7,7 @@
 *@accept:character to be located
 *Return: pointer to the first occurence of character c in string s
 */
-char _strpbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	return (*strpbrk(s, accept));
+	return (strpbrk( s, accept));
 }
