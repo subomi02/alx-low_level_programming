d8dd4705bb421402a462f23b3c10b989ef4b66fa
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-28 17:01:31 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+/**
+ * malloc_checked - allocates memory using malloc
+ *@b:Amount of memory to allocate
+ * Return: Always 0.
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	int *str;
+
+	str = malloc(b);
+	if (str == NULL)
+		exit(98);
+
+	return (str);
+}
