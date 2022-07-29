7672f2db5622b7a6fa94a0648e095ff6677e92d3
Adetunji Subomi <adetunjisubomi02@gmail.com>
2022-07-28 17:35:21 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *str;
 
-	if (nmemb == NULL || size == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	str = calloc(nmemb, size);
