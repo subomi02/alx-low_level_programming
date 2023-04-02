52a19642b402dcb37dc8e78e86ab40445943718b
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-04-02 23:06:49 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,33 +4,33 @@
 #include <string.h>
 
 /**
- * string_nconcat - concatenate the first n bytes of a string to another
- * @str1: pointer to the first string
- * @str2: pointer to the second string
- * @size: size of str2 to be concatenated
+ * string_nconcat - concatenate the first n bytes of a string
+ * @sr1: pointer to the first string
+ * @sr2: pointer to the second string
+ * @size: size of sr2 to be concatenated
  *
  * Return: pointer to the concatenated string
  */
 
-char *string_nconcat(char *str1, char *str2, unsigned int size)
+char *string_nconcat(char *sr1, char *sr2, unsigned int size)
 {
 	unsigned int len1, len2;
-	char *ptr;
+	char *pr;
 
-	if (str1 == NULL)
-		str1 = "";
-	if (str2 == NULL)
-		str2 = "";
+	if (sr1 == NULL)
+		sr1 = "";
+	if (sr2 == NULL)
+		sr2 = "";
 
-	len1 = strlen(str1);
-	len2 = strlen(str2) <= size ? strlen(str2) : size;
-	ptr = malloc(len1 + len2 + 1);
+	len1 = strlen(sr1);
+	len2 = strlen(sr2) <= size ? strlen(sr2) : size;
+	pr = malloc(len1 + len2 + 1);
 
-	if (ptr == NULL)
+	if (pr == NULL)
 		return (NULL);
 
-	strcpy(ptr, str1);
-	strncat(ptr, str2, size);
+	strcpy(pr, sr1);
+	strncat(pr, sr2, size);
 
-	return (ptr);
+	return (pr);
 }
